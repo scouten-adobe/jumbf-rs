4c3d4b21f636dbec629d98b6e592d5529681827d
Eric Scouten <git@scouten.me>
2026-01-24 11:17:27 -0800

    chore: Move tests inline with code under test

--- src/builder/data_box_builder.rs
+++ src/builder/data_box_builder.rs
@@ -73,3 +73,74 @@ impl<'a> ToBox for DataBoxBuilder<'a> {
         to_stream.write_all(&self.data)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #![allow(clippy::expect_used)]
+    #![allow(clippy::panic)]
+    #![allow(clippy::unwrap_used)]
+
+    use std::io::Cursor;
+
+    use hex_literal::hex;
+
+    use crate::{
+        box_type::DESCRIPTION_BOX_TYPE,
+        builder::{
+            to_box::{jumbf_size, write_jumbf},
+            DataBoxBuilder, ToBox,
+        },
+    };
+
+    #[test]
+    fn simple_box_borrowed() {
+        let expected_jumbf = hex!(
+            "00000026" // box size
+            "6a756d64" // box type = 'jumd'
+            "00000000000000000000000000000000" // UUID
+            "03" // toggles
+            "746573742e64657363626f7800" // label
+        );
+
+        let boxx = DataBoxBuilder::from_borrowed(DESCRIPTION_BOX_TYPE, &expected_jumbf[8..]);
+
+        assert_eq!(boxx.box_type(), DESCRIPTION_BOX_TYPE);
+        assert_eq!(boxx.payload_size().unwrap(), 30);
+
+        let mut payload = Cursor::new(Vec::<u8>::new());
+        boxx.write_payload(&mut payload).unwrap();
+        assert_eq!(*payload.into_inner(), expected_jumbf[8..]);
+
+        assert_eq!(jumbf_size(&boxx).unwrap(), 38);
+
+        let mut jumbf = Cursor::new(Vec::<u8>::new());
+        write_jumbf(&boxx, &mut jumbf).unwrap();
+        assert_eq!(*jumbf.into_inner(), expected_jumbf);
+    }
+
+    #[test]
+    fn simple_box_owned() {
+        let expected_jumbf = hex!(
+            "00000026" // box size
+            "6a756d64" // box type = 'jumd'
+            "00000000000000000000000000000000" // UUID
+            "03" // toggles
+            "746573742e64657363626f7800" // label
+        );
+
+        let boxx = DataBoxBuilder::from_owned(DESCRIPTION_BOX_TYPE, expected_jumbf[8..].to_owned());
+
+        assert_eq!(boxx.box_type(), DESCRIPTION_BOX_TYPE);
+        assert_eq!(boxx.payload_size().unwrap(), 30);
+
+        let mut payload = Cursor::new(Vec::<u8>::new());
+        boxx.write_payload(&mut payload).unwrap();
+        assert_eq!(*payload.into_inner(), expected_jumbf[8..]);
+
+        assert_eq!(jumbf_size(&boxx).unwrap(), 38);
+
+        let mut jumbf = Cursor::new(Vec::<u8>::new());
+        write_jumbf(&boxx, &mut jumbf).unwrap();
+        assert_eq!(*jumbf.into_inner(), expected_jumbf);
+    }
+}
